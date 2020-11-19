dc7d2fe29d79d22f76b802f47cf5d710a349525a
Matt <39469614+KosmicShovel@users.noreply.github.com>
2020-11-19 11:01:28 -0600

    1.1 check description for details
    Added support for C# and C++ as output types, removed unneccessary typedef keyword, added -lang command, added O2 optimization.

--- main.cpp
+++ main.cpp
@@ -2,15 +2,20 @@
 #include "main.h"
 #include <string>
 #include <conio.h>
+#include <algorithm>
+#include <cctype>
+#include <string>
+#include <vector>
 
 #define string std::string
 #define start "-start"
-#define end "-end"
+#define endM "-end"
 #define steps "-steps"
 #define listName "-name"
+#define language "-lang"
 
 int main(int argc, char* argv[]) {
-    if (argc < 9){
+    if (argc < 11){
         ColorGen::showUsage();
         return 1;
     }
@@ -19,6 +24,7 @@ int main(int argc, char* argv[]) {
     RGBSet_s* startc = new RGBSet_s{0, 0, 0};
     RGBSet_s* endc = new RGBSet_s{0, 0, 0};
     const char* listNamec = "colorIdForTick";
+    string langc = "java";
     string delimiter = ",";
 
     for (int i = 1; i < argc; ++i) {
@@ -41,6 +47,8 @@ int main(int argc, char* argv[]) {
                     if (!blue) {startc->blue = atoi(s.c_str());blue = 1;}
                 } else if (s.find("0x") != string::npos) {
                     sscanf_s(s.c_str(), "0x%02x%02x%02x", &startc->red, &startc->green, &startc->blue);
+                } else if (s.find("#") != string::npos) {
+                    sscanf_s(s.c_str(), "#%02x%02x%02x", &startc->red, &startc->green, &startc->blue);
                 } else {
                     sscanf_s(s.c_str(), "%02x%02x%02x", &startc->red, &startc->green, &startc->blue);
                 }
@@ -48,7 +56,7 @@ int main(int argc, char* argv[]) {
                 ColorGen::showUsage();
                 return 1;
             }
-        } else if (arg == end) {
+        } else if (arg == endM) {
             if (i + 1 < argc) {
                 string s = argv[++i];
                 if (s.find(delimiter) != string::npos) {
@@ -66,6 +74,8 @@ int main(int argc, char* argv[]) {
                     if (!blue) {endc->blue = atoi(s.c_str());blue = 1;}
                 } else if (s.find("0x") != string::npos) {
                     sscanf_s(s.c_str(), "0x%02x%02x%02x", &endc->red, &endc->green, &endc->blue);
+                } else if (s.find("#") != string::npos) {
+                    sscanf_s(s.c_str(), "#%02x%02x%02x", &endc->red, &endc->green, &endc->blue);
                 } else {
                     sscanf_s(s.c_str(), "%02x%02x%02x", &endc->red, &endc->green, &endc->blue);
                 }
@@ -83,25 +93,34 @@ int main(int argc, char* argv[]) {
             }
         } else if (arg == listName) {
             listNamec = argv[++i];
+        } else if (arg == language) {
+            string s = argv[++i];
+            if (s.c_str()) {
+                std::transform(s.begin(), s.end(), s.begin(), [](char c){return static_cast<char>(std::tolower(c));});
+
+                if ((s.find("java") != string::npos) || (s.find("c#") != string::npos) ||
+                    (s.find("cs") != string::npos) || (s.find("cpp") != string::npos) ||
+                    (s.find("c++") != string::npos)) {
+                    langc = s;
+                } else {
+                    std::cerr << "Can't detect language! - " << s << std::endl;
+                    ColorGen::showUsage();
+                    return 1;
+                }
+            }
         }
     }
 
-    std::cout << "\tprivate static ArrayList<Integer> " << listNamec << " = new ArrayList<Integer>();\n\n\tstatic {\n";
-    for (int i = 0; i <= stepsc; ++i) {
-        float percent = (float)i / stepsc;
-        char hex[16];
-        RGBSet_s* interpolated = ColorGen::interpolate(startc, endc, percent);
-        sprintf(hex, "0x%02X%02X%02X", interpolated->red, interpolated->green, interpolated->blue);
-        std::cout << "\t\t" << listNamec << ".add(" << hex << ");\n";
-    }
-    std::cout << "\t}";
+
+    ColorGen::outputGeneration(langc, startc, endc, stepsc, listNamec);
+
     std::cout << "Press any key to continue..." << std::endl;
     _getch();
     return 0;
 }
 
 void ColorGen::showUsage() {
-    std::cerr << "Options:\n\t-start Color code for start color, (0xFF00AA, FF11AA, 255,14,60)\n\t-end Color code for end color, (0xFF00AA, FF11AA, 255,14,60)\n\t-steps amount of steps to take to transition from start to end\n\t-name name of the arraylist" << std::endl;
+    std::cerr << "Options:\n\t-start Color code for start color, (#00FF00, 0xFF00AA, FF11AA, 255,14,60)\n\t-end Color code for end color, (#00FF00, 0xFF00AA, FF11AA, 255,14,60)\n\t-steps amount of steps to take to transition from start to end\n\t-name name of the arraylist\n\t-lang Language to output as (Java, CS, CPP)" << std::endl;
     std::cout << "Press any key to continue..." << std::endl;
     _getch();
     return;
@@ -121,4 +140,55 @@ RGBSet_s* ColorGen::interpolate(RGBSet_s* startingColor, RGBSet_s* endColor, flo
     int   b3 = (int) ((b2 - b1) * percent + b1);
 
     return new RGBSet_s{r3, g3, b3};
+}
+
+void ColorGen::outputGeneration(string langc, RGBSet_s* startColor, RGBSet_s* endColor, int stepsc, string lName) {
+    if (langc.find("java") != string::npos) {
+        std::cout << "\tprivate static ArrayList<Integer> " << lName << " = new ArrayList<Integer>();\n\n\tstatic {\n";
+        for (int i = 0; i <= stepsc; ++i) {
+            float percent = (float)i / stepsc;
+            char hex[16];
+            RGBSet_s* interpolated = ColorGen::interpolate(startColor, endColor, percent);
+            sprintf_s(hex, "0x%02X%02X%02X", interpolated->red, interpolated->green, interpolated->blue);
+            std::cout << "\t\t" << lName << ".add(" << hex << ");\n";
+        }
+        std::cout << "\t}\n";
+        return;
+    }
+
+    if ((langc.find("cs") != string::npos) || (langc.find("c#") != string::npos)) {
+        std::cout << "\t\tpublic static List<int> " << lName << " = new List<int> {" << std::endl;
+        char hex[16];
+        sprintf_s(hex, "0x%02X%02X%02X", startColor->red, startColor->green, startColor->blue);
+        std::cout << "\t\t\t " << hex;
+        int lastI = 0;
+        for (int i = 0; i <= stepsc; ++i) {
+            float percent = (float)i / stepsc;
+            char hexI[16];
+            RGBSet_s* interpolated = ColorGen::interpolate(startColor, endColor, percent);
+            sprintf_s(hexI, "0x%02X%02X%02X", interpolated->red, interpolated->green, interpolated->blue);
+            std::cout << (i % 2 == 0 ? ", " : "\t\t\t ") << hexI << (i == stepsc ? "\n" : (i % 2 == 0 ? ",\n" : ""));
+            lastI = i;
+        }
+        std::cout << (lastI % 2 == 1 ? "\n" : "") << "\t\t};" << std::endl;
+        return;
+    }
+
+    if ((langc.find("cpp") != string::npos) || (langc.find("c++") != string::npos)) {
+        std::cout << "\tstd::vector<int> " << lName << " = std::vector<int>{" << std::endl;
+        char hex[16];
+        sprintf_s(hex, "0x%02X%02X%02X", startColor->red, startColor->green, startColor->blue);
+        std::cout << "\t\t " << hex;
+        int lastI = 0;
+        for (int i = 0; i <= stepsc; ++i) {
+            float percent = (float)i / stepsc;
+            char hexI[16];
+            RGBSet_s* interpolated = ColorGen::interpolate(startColor, endColor, percent);
+            sprintf_s(hexI, "0x%02X%02X%02X", interpolated->red, interpolated->green, interpolated->blue);
+            std::cout << (i % 2 == 0 ? ", " : "\t\t ") << hexI << (i == stepsc ? "\n" : (i % 2 == 0 ? ",\n" : ""));
+            lastI = i;
+        }
+        std::cout << (lastI % 2 == 1 ? "\n" : "") << "\t};" << std::endl;
+        return;
+    }
 }
--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef ColorGen_MAIN_H
 #define ColorGen_MAIN_H
 
-typedef struct RGBSet_s {
+struct RGBSet_s {
 public:
     int red;
     int green;
@@ -12,6 +12,7 @@ class ColorGen {
     public:
         static RGBSet_s* interpolate(RGBSet_s* startingColor, RGBSet_s* endColor, float percent);
         static void showUsage();
+        static void outputGeneration(std::string langc, RGBSet_s* starColor, RGBSet_s* endColor, int stepsc, std::string lName);
 };
 
 #endif
